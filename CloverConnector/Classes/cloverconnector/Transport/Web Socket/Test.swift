6455dc12e7cb5184a61bf27f86ed58f92d6f75e2
smlgroot <smlgroot@gmail.com>
2022-05-10 19:07:06 -0500

    test15

--- CloverConnector/Classes/cloverconnector/Transport/Web Socket/Test.swift
+++ CloverConnector/Classes/cloverconnector/Transport/Web Socket/Test.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 public class Test : PairingDeviceConfiguration {
-    public func onPairingCode(pairingCode: String) {
+    public func onPairingCode(_ pairingCode: String) {
         // display pairingCode to user, to be entered on the Clover Mini
     }
 
-    public func onPairingSuccess(authToken: String) {
+    public func onPairingSuccess(_ authToken: String) {
         // pairing is successful
         // save this authToken to pass in to the config for future connections
         // so pairing will happen automatically
